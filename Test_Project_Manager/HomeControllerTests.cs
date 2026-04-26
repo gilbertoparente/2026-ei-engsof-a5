1b33bb40883999f3794fdc0dae3e6ed072229d07
Rubenlima2001 <LIMARUBEN@IPVC.PT>
2026-04-27 00:06:02 +0100

    Testes unitarios para os Controladores:Categoriatelento,Home,Cliente,Conta,Pesquisa,Propostas e Talentos

---
+++ Test_Project_Manager/HomeControllerTests.cs
@@ -0,0 +1,15 @@
+namespace Test_Project_Manager;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
